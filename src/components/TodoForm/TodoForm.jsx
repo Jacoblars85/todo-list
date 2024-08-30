22d924a2240d0053dee714cf3831fb0eecb82459
Jacob Larson <hockeybobber@comcast.net>
2024-08-29 21:27:23 -0500

    added form and inputs

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -4,11 +4,31 @@ import { useDispatch } from 'react-redux';
 const TodoForm = () => {
     const dispatch = useDispatch();
 
+    const [item, setItem] = useState('')
+      
+    const addItem = (e) => {
+        e.preventDefault();
+    
+        dispatch({
+          type: 'CREATE_AIRLINE',
+          payload: item
+        })
+        setItem('')
+      }
+
     return (
         <div>
             <h3>This is the form</h3>
 
-            <p>type stuff here</p>
+            <form onSubmit={addItem}>
+        <input 
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+          placeholder='Todo Name'
+        />
+      <button>Add Todo</button>
+      </form>
+
         </div>
     );
 }
