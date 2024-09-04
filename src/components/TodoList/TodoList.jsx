bf1c01f8c316f3d908cb6edd5c8f9348b70db00b
Jacob Larson <hockeybobber@comcast.net>
2024-09-03 22:45:48 -0500

    added  a useSelector for todo list

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -5,15 +5,18 @@ import { useDispatch, useSelector} from 'react-redux';
 function TodoList() {
     const dispatch = useDispatch();
 
+    const todoList = useSelector((store) => store.todoList);
+
     useEffect(() => {
 
     }, []); 
+
 
     return (
         <div>
             <h3>This is the todo list</h3>
 
-            <p>this is the todo</p>
+            <p></p>
         </div>
     );
 }
