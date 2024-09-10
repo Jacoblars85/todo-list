27d1b18dc943b0fefadc2c8ced29503385e51018
Jacob Larson <hockeybobber@comcast.net>
2024-09-09 19:21:47 -0500

    mapped through todo list

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -7,15 +7,25 @@ function TodoList() {
 
     const todoList = useSelector((store) => store.todoReducer.todoList);
 
-    useEffect(() => {
-        dispatch({ type: "SAGA_FETCH_TODO_LIST" });
-      }, []); 
+    // useEffect(() => {
+    //     dispatch({ type: "SAGA_FETCH_TODO_LIST" });
+    //   }, []); 
 
+    console.log('todoList', todoList);
+    
     return (
         <div>
             <h3>This is the todo list</h3>
 
-            <p>{todoList}</p>
+            {todoList.map((todo) => {
+            return (
+              <div key={todo.id}>
+            <p>{todo.name}</p>
+            <p>{todo.is_complete}</p>
+              </div>
+            );
+          })}
+
         </div>
     );
 }
