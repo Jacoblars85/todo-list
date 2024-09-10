6356d78d128e4da6661f02f7fa1d30319d3b63e4
Jacob Larson <hockeybobber@comcast.net>
2024-09-09 19:22:20 -0500

    formatted doc

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,33 +1,31 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector} from 'react-redux';
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function TodoList() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const todoList = useSelector((store) => store.todoReducer.todoList);
+  const todoList = useSelector((store) => store.todoReducer.todoList);
 
-    // useEffect(() => {
-    //     dispatch({ type: "SAGA_FETCH_TODO_LIST" });
-    //   }, []); 
+  // useEffect(() => {
+  //     dispatch({ type: "SAGA_FETCH_TODO_LIST" });
+  //   }, []);
 
-    console.log('todoList', todoList);
-    
-    return (
-        <div>
-            <h3>This is the todo list</h3>
+  console.log("todoList", todoList);
 
-            {todoList.map((todo) => {
-            return (
-              <div key={todo.id}>
+  return (
+    <div>
+      <h3>This is the todo list</h3>
+
+      {todoList.map((todo) => {
+        return (
+          <div key={todo.id}>
             <p>{todo.name}</p>
             <p>{todo.is_complete}</p>
-              </div>
-            );
-          })}
-
-        </div>
-    );
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default TodoList;
