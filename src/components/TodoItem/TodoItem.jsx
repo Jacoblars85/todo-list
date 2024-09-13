28080c6dd6963e3e3ae382830a1d3cfe5ae7e9d1
Jacob Larson <hockeybobber@comcast.net>
2024-09-13 16:57:07 -0500

    formatted doc

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -7,29 +7,33 @@ function TodoItem({ todo }) {
   const todoList = useSelector((store) => store.todoReducer.todoList);
 
   const finishTodo = () => {
-    console.log('finishing');
-    
+    console.log("finishing");
+
     dispatch({
-        type: "SAGA_FINISH_TODO",
-        payload: todo.id,
-      });
+      type: "SAGA_FINISH_TODO",
+      payload: todo.id,
+    });
   };
 
   const deleteTodo = () => {
-    console.log('deleting');
-    
+    console.log("deleting");
+
     dispatch({
-        type: "SAGA_DELETE_TODO",
-        payload: todo.id,
-      });
+      type: "SAGA_DELETE_TODO",
+      payload: todo.id,
+    });
   };
 
   return (
     <div>
-            <p>Task: {todo.name}</p>
-            <p>Finished: {todo.is_complete ? "complete" : "not complete"}</p>
-            {todo.is_complete ? "completed" : <button onClick={finishTodo}>complete</button>}
-            <button onClick={deleteTodo}>delete</button>
+      <p>Task: {todo.name}</p>
+      <p>Finished: {todo.is_complete ? "complete" : "not complete"}</p>
+      {todo.is_complete ? (
+        "completed"
+      ) : (
+        <button onClick={finishTodo}>complete</button>
+      )}
+      <button onClick={deleteTodo}>delete</button>
     </div>
   );
 }
