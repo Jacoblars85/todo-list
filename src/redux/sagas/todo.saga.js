c4f5587503772648d0082be9d59e8b0a7a8400a0
Jacob Larson <hockeybobber@comcast.net>
2024-09-12 22:22:36 -0500

    added a post function for the saga

--- src/redux/sagas/todo.saga.js
+++ src/redux/sagas/todo.saga.js
@@ -15,8 +15,26 @@ function* fetchTodoList() {
   }
 }
 
+function* postNewTodo(action) {
+    // console.log('action', action.payloaad);
+    try {
+      const postTodoResponse = yield axios({
+        method: 'POST',
+        url: '/api/todo/new',
+        data: action.payload
+      })
+      yield put({
+        type: 'SAGA_FETCH_TODO_LIST',
+      })
+    } catch (error) {
+      console.log('Unable to posting new todo to server', error);
+    }
+  }
+
 function* characterSaga() {
   yield takeLatest('SAGA_FETCH_TODO_LIST', fetchTodoList);
+  yield takeLatest('SAGA_POST_NEW_TODO', postNewTodo);
+
 }
 
 export default characterSaga;
