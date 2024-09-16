9c6654ef610020a0840cece2b30af4018e79e594
Jacob Larson <hockeybobber@comcast.net>
2024-09-16 18:42:41 -0500

    added id to the dummy data

--- src/redux/reducers/todo.reducer.js
+++ src/redux/reducers/todo.reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-const todoList = (state = [{name: 'pooooo', is_complete: false}, {name: 'peeee', is_complete: true}, {name: 'fart', is_complete: false}], action) => {
+const todoList = (state = [{id: 1, name: 'pooooo', is_complete: false}, {id: 2, name: 'peeee', is_complete: true}, {id: 3, name: 'fart', is_complete: false}], action) => {
   switch (action.type) {
     case 'SET_TODO_LIST':
       return action.payload;
