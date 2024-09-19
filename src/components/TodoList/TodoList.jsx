9c654e05a49047c4d40f717c025cb446f5e829da
Jacob Larson <hockeybobber@comcast.net>
2024-09-18 19:41:43 -0500

    imported todo list css

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import TodoItem from "../TodoItem/TodoItem";
+import "./TodoList.css";
 
 function TodoList() {
   const dispatch = useDispatch();
