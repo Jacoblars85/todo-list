661b4b92b7d202534f7840548a2c435cf1838aff
Jacob Larson <hockeybobber@comcast.net>
2024-09-20 14:34:37 -0500

    deleted the dummy data

--- src/redux/reducers/todo.reducer.js
+++ src/redux/reducers/todo.reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-const todoList = (state = [{id: 1, name: 'pooooo', is_complete: false}, {id: 2, name: 'peeee', is_complete: true}, {id: 3, name: 'fart', is_complete: false}], action) => {
+const todoList = (state = [{}], action) => {
   switch (action.type) {
     case 'SET_TODO_LIST':
       return action.payload;
